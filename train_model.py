32e842dfc3f2b586237ff45795f0c4790e7919b8
ProjetDataScience <elrhoddanih@gmail.com>
2025-04-21 14:06:27 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,6 +3,7 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.svm import SVC
+from imblearn.over_sampling import SMOTE
 
 # Charger les données
 df = pd.read_csv('data\churn.csv', index_col="customerID")
@@ -24,16 +25,21 @@ y = df_dummies['Churn']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Rééchantillonnage des données d'entraînement
+
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+
 ## Normalisation des données
 
 scaler = StandardScaler()
 
-X_train[['tenure', 'MonthlyCharges', 'TotalCharges']] = scaler.fit_transform(X_train[['tenure', 'MonthlyCharges', 'TotalCharges']])
-X_test[['tenure', 'MonthlyCharges', 'TotalCharges']] = scaler.transform(X_test[['tenure', 'MonthlyCharges', 'TotalCharges']])
+X_resampled[['tenure', 'MonthlyCharges', 'TotalCharges']] = scaler.fit_transform(X_resampled[['tenure', 'MonthlyCharges', 'TotalCharges']])
+X_resampled[['tenure', 'MonthlyCharges', 'TotalCharges']] = scaler.transform(X_resampled[['tenure', 'MonthlyCharges', 'TotalCharges']])
 
-# Entrainer le modèle 
-model = SVC(C=0.1, gamma=1, kernel='linear')
-model.fit(X_train, y_train)
+# Entraînement du modèle sur les données équilibrées
+model = SVC()
+model.fit(X_resampled, y_resampled)
 
 # Sauvegarder le modèle et le scaler
 joblib.dump(model, 'model/churn_model.pkl')
